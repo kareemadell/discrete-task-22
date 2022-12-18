9e94961660577efb358799f0bd28c48c77305cb8
kareemadell <114925509+kareemadell@users.noreply.github.com>
2022-12-18 21:46:18 +0200

    Add files via upload

---
+++ perfect num/perfect num/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perfect_num
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter big number");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("enter small number");
+            double num2 = double.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+            { while(num1 > num2) {
+
+                 if (  num2 % 2==0 )
+                    {
+                        Console.WriteLine(num2);
+                    }
+                    num2 += 1;
+
+                }
+
+            }
+            else Console.WriteLine("you have error ");
+
+
+
+        }
+    }
+}
---
+++ prime num/prime num/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime_num
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter big number");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("enter small number");
+            double num2 = double.Parse(Console.ReadLine());
+            
+
+
+
+
+              if (num1 > num2)
+              {
+                  while (num1 > num2)
+                  {
+
+                     if (num2 % 2 != 0 && num2 % 3 != 0 && num2 % 4 != 0 && num2 % 5 != 0 && num2 % 6 != 0
+                                && num2 % 9 != 0 && num2 % 8 != 0 && num2 % 7 != 0 )
+
+           
+                      {
+                          Console.WriteLine(num2);
+                      }
+                      num2 += 1;
+
+                  }
+
+              }
+              else Console.WriteLine("you have error ");
+
+        }
+    }
+}
